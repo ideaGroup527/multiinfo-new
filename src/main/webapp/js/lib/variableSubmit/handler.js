25b0b9f83d8f8d1b83be8a2569202609d16ff788
vango <daikaiquan46@gmail.com>
2016-04-17 10:44:24 +0800

    描述统计制表

--- src/main/webapp/js/lib/variableSubmit/handler.js
+++ src/main/webapp/js/lib/variableSubmit/handler.js
@@ -3,6 +3,16 @@ var handleSubmitType = function (type, data) {
 
         case 'Descriptive_Statistics':
             //描述统计 - 描述
+            var configList = $('#Descriptive_Statistics_Config input[type=checkbox]:checked');
+            var configs = [];
+            configList.map(function (i, config) {
+                console.log(i, config);
+                configs.push($(config).val());
+            });
+
+            console.log(configs);
+            sessionStorage.setItem('PRIVATE_CONFIG', configs);
+
             var variableChosed = $('#descriptive_statistics_variables_area .active');
 
             var variableList = [];
@@ -10,17 +20,17 @@ var handleSubmitType = function (type, data) {
                 var params = JSON.parse($(variable).attr('data-config'));
                 variableList.push(params);
             });
-            console.log(variableList);
-            var da = {
-                    'variableList': variableList,
-                    'dataGrid': data
-                }
-            console.log(da);
+
+            var data = {
+                'variableList': variableList,
+                'dataGrid': data
+            };
 
             $.ajax({
                 url: 'statistics/descriptives.do?method=descriptives',
                 //url: 'statistics/descriptives.do?method=frequency',
-                data: JSON.stringify(da),
+                //url: 'js/testJSON/result-table.json',
+                data: JSON.stringify(data),
                 //async: false,
                 //processData: false,
                 contentType: 'application/json',
@@ -28,8 +38,11 @@ var handleSubmitType = function (type, data) {
                 dataType: 'JSON',
                 success: function (data) {
                     console.log(data);
+                    sessionStorage.setItem('PRIVATE_TABLE_RESULT', JSON.stringify(data));
                 }
             });
+
+            window.location.href = 'result-display.html';
 
             break;
     }
