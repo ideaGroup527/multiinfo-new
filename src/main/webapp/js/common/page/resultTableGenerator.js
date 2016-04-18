b3c3ba640ab9c66eebecdfcded07c816578a2bd7
vango <daikaiquan46@gmail.com>
2016-04-18 21:09:21 +0800

    描述统计 - 描述（bug 修复）

--- src/main/webapp/js/common/page/resultTableGenerator.js
+++ src/main/webapp/js/common/page/resultTableGenerator.js
@@ -2,112 +2,24 @@ var resultTableGenerator = function () {
 
     var def = $.Deferred();
 
-    var presentArea = $('.result-table');
-    console.log(presentArea);
-
     var resultType = sessionStorage.getItem('PRIVATE_RESULT_TYPE');
     var tableResult = JSON.parse(sessionStorage.getItem('PRIVATE_TABLE_RESULT'));
 
     switch (resultType) {
         case 'Descriptive_Statistics_Descriptive':
-            handleDescriptiveStatisticsDescriptive();
+            handleDescriptiveStatisticsDescriptive(tableResult);
             break;
         case 'Descriptive_Statistics_Frequency':
-            var frequencyData = tableResult.resDataMap;
-            for (var variable in frequencyData) {
-                var container = $('<div>');
-
-                var title = $('<h1>');
-                $(title).text(variable);
-                $(container).append(title);
-
-                var variableTable = $('<table>');
-                $(variableTable).addClass('table table-striped table-bordered active');
-
-                var tr = $('<tr>');
-
-                var emptyCell = $('<th>');
-                $(tr).append(emptyCell);
-
-                ['label_frequency', 'label_percentage', 'label_validate_percentage', 'label_accumulation_percentage'].map(function (header, i) {
-                    var th = $('<th>');
-                    $(th).attr('data-i18n-type', 'page')
-                        .attr('data-i18n-tag', header);
-                    $(tr).append(th);
-                });
-
-                $(variableTable).append(tr);
-                var uniqueData = frequencyData[variable].resultData.uniqueData;
-                var frequency = frequencyData[variable].resultData.frequencyMap;
-                var percentage = frequencyData[variable].resultData.percentage;
-                var accumulationPercentage = frequencyData[variable].resultData.accumulationPercentage;
-
-                uniqueData.map(function (unique, i) {
-                    var tr = $('<tr>');
-
-                    var dataCell = $('<td>');
-                    $(dataCell).text(unique);
-                    $(tr).append(dataCell);
-
-                    var frequencyCell = $('<td>');
-                    $(frequencyCell).text(frequency[unique]);
-                    $(tr).append(frequencyCell);
-
-                    var percentageCell = $('<td>');
-                    $(percentageCell).text(percentage[unique]);
-                    $(tr).append(percentageCell);
-
-                    var validatePercentageCell = $('<td>');
-                    $(validatePercentageCell).text(percentage[unique]);
-                    $(tr).append(validatePercentageCell);
-
-                    var accumulationPercentageCell = $('<td>');
-                    $(accumulationPercentageCell).text(accumulationPercentage[unique]);
-                    $(tr).append(accumulationPercentageCell);
-
-                    $(variableTable).append(tr);
-                });
-
-                var lastRow = $('<tr>');
-
-                var lastRow_1_Cell = $('<th>');
-                $(lastRow_1_Cell).attr('data-i18n-type', 'page')
-                    .attr('data-i18n-tag', 'label_total');
-
-                var frequencyTotal = 0, percentageTotal = 0;
-                uniqueData.map(function (data) {
-                    frequencyTotal += frequency[data];
-                    percentageTotal += percentage[data];
-                });
-
-                var lastRow_2_Cell = $('<th>');
-                $(lastRow_2_Cell).text(frequencyData[variable].resultData.sumFreq);
-
-                var lastRow_3_Cell = $('<th>');
-                $(lastRow_3_Cell).text(frequencyData[variable].resultData.sumPercentage);
-
-                var lastRow_4_Cell = $('<th>');
-                $(lastRow_4_Cell).text(frequencyData[variable].resultData.sumValidatePercentage);
-
-                var lastRow_5_Cell = $('<th>');
-                $(lastRow).append(lastRow_1_Cell)
-                    .append(lastRow_2_Cell)
-                    .append(lastRow_3_Cell)
-                    .append(lastRow_4_Cell)
-                    .append(lastRow_5_Cell);
-
-                $(variableTable).append(lastRow);
-
-                $(container).append(variableTable);
-                $(presentArea).append(container);
-            }
+            handleDescriptiveStatisticsFrequency(tableResult);
             break;
     }
 
     return def.resolve().promise();
 };
 
-var handleDescriptiveStatisticsDescriptive = function () {
+var handleDescriptiveStatisticsDescriptive = function (tableResult) {
+
+    var presentArea = $('.result-table');
 
     var algorithmConfigs = sessionStorage.getItem('PRIVATE_ALGORITHM_CONFIG').split(',');
     var graphConfigs = sessionStorage.getItem('PRIVATE_GRAPH_CONFIG').split(',');
@@ -182,3 +94,99 @@ var handleDescriptiveStatisticsDescriptive = function () {
         });
     }
 };
+
+
+var handleDescriptiveStatisticsFrequency = function (tableResult) {
+
+    var presentArea = $('.result-table');
+
+    var frequencyData = tableResult.resDataMap;
+    for (var variable in frequencyData) {
+        var container = $('<div>');
+        $(container).addClass('frequency-table');
+
+        var title = $('<h1>');
+        $(title).text(variable);
+        $(container).append(title);
+
+        var variableTable = $('<table>');
+        $(variableTable).addClass('table table-striped table-bordered active');
+
+        var tr = $('<tr>');
+
+        var emptyCell = $('<th>');
+        $(tr).append(emptyCell);
+
+        ['label_frequency', 'label_percentage', 'label_validate_percentage', 'label_accumulation_percentage'].map(function (header, i) {
+            var th = $('<th>');
+            $(th).attr('data-i18n-type', 'page')
+                .attr('data-i18n-tag', header);
+            $(tr).append(th);
+        });
+
+        $(variableTable).append(tr);
+        var uniqueData = frequencyData[variable].resultData.uniqueData;
+        var frequency = frequencyData[variable].resultData.frequencyMap;
+        var percentage = frequencyData[variable].resultData.percentage;
+        var accumulationPercentage = frequencyData[variable].resultData.accumulationPercentage;
+
+        uniqueData.map(function (unique, i) {
+            var tr = $('<tr>');
+
+            var dataCell = $('<td>');
+            $(dataCell).text(unique);
+            $(tr).append(dataCell);
+
+            var frequencyCell = $('<td>');
+            $(frequencyCell).text(frequency[unique]);
+            $(tr).append(frequencyCell);
+
+            var percentageCell = $('<td>');
+            $(percentageCell).text(percentage[unique]);
+            $(tr).append(percentageCell);
+
+            var validatePercentageCell = $('<td>');
+            $(validatePercentageCell).text(percentage[unique]);
+            $(tr).append(validatePercentageCell);
+
+            var accumulationPercentageCell = $('<td>');
+            $(accumulationPercentageCell).text(accumulationPercentage[unique]);
+            $(tr).append(accumulationPercentageCell);
+
+            $(variableTable).append(tr);
+        });
+
+        var lastRow = $('<tr>');
+
+        var lastRow_1_Cell = $('<th>');
+        $(lastRow_1_Cell).attr('data-i18n-type', 'page')
+            .attr('data-i18n-tag', 'label_total');
+
+        var frequencyTotal = 0, percentageTotal = 0;
+        uniqueData.map(function (data) {
+            frequencyTotal += frequency[data];
+            percentageTotal += percentage[data];
+        });
+
+        var lastRow_2_Cell = $('<th>');
+        $(lastRow_2_Cell).text(frequencyData[variable].resultData.sumFreq);
+
+        var lastRow_3_Cell = $('<th>');
+        $(lastRow_3_Cell).text(frequencyData[variable].resultData.sumPercentage);
+
+        var lastRow_4_Cell = $('<th>');
+        $(lastRow_4_Cell).text(frequencyData[variable].resultData.sumValidatePercentage);
+
+        var lastRow_5_Cell = $('<th>');
+        $(lastRow).append(lastRow_1_Cell)
+            .append(lastRow_2_Cell)
+            .append(lastRow_3_Cell)
+            .append(lastRow_4_Cell)
+            .append(lastRow_5_Cell);
+
+        $(variableTable).append(lastRow);
+
+        $(container).append(variableTable);
+        $(presentArea).append(container);
+    }
+}
