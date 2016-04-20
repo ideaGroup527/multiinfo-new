8acb3ba1444c99a9d8f68ac017849ce04d95a554
bigtage <896932646@qq.com>
2016-04-20 23:16:28 +0800

    适配多维的直方图搞定了

--- src/main/webapp/js/charts/bar.js
+++ src/main/webapp/js/charts/bar.js
@@ -3,19 +3,65 @@ function Bar(config) {
 
     this.format = function (data) {//转换格式
 
-        var dataAll = {k: [],v:[]};
+        var dataAll = {xAxis: [], sub_key: [], value: []};
 
-        for(var key in data.resDataMap){
-            dataAll.k.push(key);
-            dataAll.v.push(data.resDataMap[key].resultData.total);
+        for (var key in data.resDataMap) {
+            dataAll.xAxis.push(key);
         }
+
+        var sub = 0, iSub_key = 0;
+        for (var key in data.resDataMap) {
+            if (data.resDataMap[key].resultData != null) {
+
+                sub = iSub_key = 1;
+                break;
+            }
+            if (sub == 1) {
+                break;
+            }
+
+            for (var sub_key in data.resDataMap[key]) {
+                dataAll.sub_key.push(sub_key);
+                sub = 1;
+            }
+        }
+        if (iSub_key == 1) {
+
+            var value = {};
+            value.name = data.reportTitle;
+            value.type = "bar";
+            var dd = [];
+            for (var key in data.resDataMap) {
+                dd.push(data.resDataMap[key].resultData.total);
+            }
+            value.data = dd;
+            dataAll.value.push(value);
+        } else {
+
+            for (var i = 0, v = null; (v = dataAll.sub_key[i]) != undefined; i++) {
+                var value = {};
+                value.name = v;
+                value.type = "bar";
+                var dd = [];
+                for (var key in data.resDataMap) {
+                    dd.push(data.resDataMap[key][v].resultData.total);
+                }
+                value.data = dd;
+                dataAll.value.push(value);
+            }
+        }
+
+
         return dataAll;
     };
-    this.data=this.format(config.data);
+
+    this.data = this.format(config.data);
+
+    console.log(this.data);
     this.option = {
         title: {
             text: config.title, //主标题文本
-            x: 'center', //标题文本的位置
+            x: 'left', //标题文本的位置
             y: 0,
             textStyle: {
                 fontSize: 16,
@@ -31,18 +77,18 @@ function Bar(config) {
             }
         },
         legend: {
-            data:['蒸发量','降水量','平均温度']
+            data: this.data.sub_key
         },
         xAxis: [
             {
                 type: 'category',
-                data: this.format(config.data).k
+                data: this.data.xAxis
             }
         ],
         yAxis: [
             {
                 type: 'value',
-                name: config.data.reportTitle|"总量",
+                name: config.data.reportTitle | "总量",
                 min: 0,
                 max: '{dataMax}',
                 interval: 50,
@@ -51,12 +97,6 @@ function Bar(config) {
                 }
             }
         ],
-        series: [
-            {
-                name:config.data.reportTitle|"总量",
-                type:'bar',
-                data:this.data.v
-            }
-        ]
+        series: this.data.value
     };
 }
