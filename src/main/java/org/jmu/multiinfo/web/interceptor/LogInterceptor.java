b8b77e10259ea07f908a52847291f2d5010ea082
www1350 <www_1350@163.com>
2016-05-10 10:46:22 +0800

    记录ip、mac

--- src/main/java/org/jmu/multiinfo/web/interceptor/LogInterceptor.java
+++ src/main/java/org/jmu/multiinfo/web/interceptor/LogInterceptor.java
@@ -1,6 +1,9 @@
 package org.jmu.multiinfo.web.interceptor;
 
 import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.LineNumberReader;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -22,8 +25,48 @@ public class LogInterceptor extends HandlerInterceptorAdapter{
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
     	logger.debug("enter " + request.getRequestURI() + File.separator + request.getQueryString());
-
+    	String ip = getIpAddr(request);
+    	logger.debug("IP:"+ip+"MAC:"+getMACAddress(ip));
         return true;
     }
+    
+    
+    public String getIpAddr(HttpServletRequest request) {
+    	String ip = request.getHeader("x-forwarded-for");
+    	if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+    	ip = request.getHeader("Proxy-Client-IP");
+    	}
+    	if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+    	ip = request.getHeader("WL-Proxy-Client-IP");
+    	}
+    	if (ip == null || ip.length() == 0 || "unknown".equalsIgnoreCase(ip)) {
+    	ip = request.getRemoteAddr();
+    	}
+    	return ip;
+    }
+    
+    
+    public String getMACAddress(String ip) {  
+        String str = "";  
+        String macAddress = "";  
+        try {  
+            Process p = Runtime.getRuntime().exec("nbtstat -A " + ip);  
+            InputStreamReader ir = new InputStreamReader(p.getInputStream());  
+            LineNumberReader input = new LineNumberReader(ir);  
+            for (int i = 1; i < 100; i++) {  
+                str = input.readLine();  
+                if (str != null) {  
+                    if (str.indexOf("MAC Address") > 1) {  
+                        macAddress = str.substring(  
+                                str.indexOf("MAC Address") + 14, str.length());  
+                        break;  
+                    }  
+                }  
+            }  
+        } catch (IOException e) {  
+            e.printStackTrace(System.out);  
+        }  
+        return macAddress;  
+    }  
 
 }
