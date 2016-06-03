90a690e264f53cb3a251588ac1f26b0cb2fc74d9
tage <896932646@qq.com>
2016-06-04 01:34:25 +0800

    print

--- src/main/webapp/js/common/page/print.js
+++ src/main/webapp/js/common/page/print.js
@@ -1,10 +1,12 @@
-function print(printpage)
+function printData(id)
 {
     var headstr = "<html><head><title></title></head><body>";
     var footstr = "</body>";
-    var newstr = document.all.item(printpage).innerHTML;
+    var newstr = $(id).html();
+    var can = document.getElementsByTagName("canvas");
+    var img = can[0].toDataURL("image/png");
     var oldstr = document.body.innerHTML;
-    document.body.innerHTML = headstr+newstr+footstr;
+    document.body.innerHTML = headstr+newstr+'<img src="'+img+'"/>'+footstr;
     window.print();
     document.body.innerHTML = oldstr;
     return false;
